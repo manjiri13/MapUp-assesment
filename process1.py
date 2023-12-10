99a606225474ef771579d204d8fec48a34a70006
manjiri13 <manjiripathak13@gmail.com>
2023-12-10 22:28:01 +0530

    solution of 3 proccesses in python files and excel assesment

--- process1.py
+++ process1.py
@@ -4,60 +4,44 @@ from datetime import datetime, timedelta
 import os
 
 def process_gps_data(parquet_file, output_dir):
-    # Load the Parquet file into a Pandas DataFrame
     df = pd.read_parquet(parquet_file)
-
-    # Sort the DataFrame by unit and timestamp
     df.sort_values(by=['unit', 'timestamp'], inplace=True)
 
-    # Initialize variables to track trips
     current_unit = None
     trip_number = 0
     last_timestamp = None
-
-    # Iterate through rows and create trip-specific CSV files
     for index, row in df.iterrows():
         if current_unit != row['unit']:
-            # New unit encountered, reset trip information
             current_unit = row['unit']
             trip_number = 0
             last_timestamp = None
 
         if last_timestamp is not None:
-            # Convert timestamp values to datetime objects
             timestamp_current = datetime.strptime(row['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
             timestamp_last = datetime.strptime(last_timestamp, '%Y-%m-%dT%H:%M:%SZ')
 
-            # Calculate time difference in hours
             time_diff = (timestamp_current - timestamp_last).total_seconds() / 3600.0
 
-            # Check if a new trip should start
             if time_diff > 7:
                 trip_number += 1
 
-        # Update last timestamp for the next iteration
         last_timestamp = row['timestamp']
 
-        # Create a CSV file for the trip
         trip_filename = f"{current_unit}_{trip_number}.csv"
         trip_filepath = os.path.join(output_dir, trip_filename)
 
-        # Write the row to the CSV file or create a new file if it doesn't exist
         if not os.path.exists(trip_filepath):
             row.to_frame().transpose().to_csv(trip_filepath, index=False, header=True, mode='w')
         else:
             row.to_frame().transpose().to_csv(trip_filepath, index=False, header=False, mode='a')
 
 if __name__ == "__main__":
-    # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Process GPS data and extract trip information.")
     parser.add_argument("--to_process", required=True, help="Path to the Parquet file to be processed.")
     parser.add_argument("--output_dir", required=True, help="The folder to store the resulting CSV files.")
     args = parser.parse_args()
 
-    # Ensure the output directory exists, create if not
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    # Process the GPS data and create trip-specific CSV files
     process_gps_data(args.to_process, args.output_dir)
--- process2.py
+++ process2.py
@@ -2,40 +2,28 @@ import os
 import requests
 import argparse
 from concurrent.futures import ThreadPoolExecutor
-#from dotenv import load_dotenv
-
-# Load environment variables from .env file
-#load_dotenv()
 
 def upload_to_tollguru(file_path, output_dir):
-    # Get TollGuru API key and URL from environment variables
     api_key = os.getenv("TOLLGURU_API_KEY")
     api_url = os.getenv("TOLLGURU_API_URL")
 
-    # API endpoint for uploading CSV files
     endpoint_url = f"{api_url}&vehicleType=5AxlesTruck"
     
-    # Set headers
     headers = {'x-api-key': api_key, 'Content-Type': 'text/csv'}
 
-    # Prepare file name for JSON response
     response_filename = os.path.basename(file_path).replace('.csv', '.json')
     response_filepath = os.path.join(output_dir, response_filename)
 
     with open(file_path, 'rb') as file:
-        # Send a POST request to TollGuru API
         response = requests.post(endpoint_url, data=file, headers=headers)
 
-        # Save the JSON response to the specified output directory
         with open(response_filepath, 'w') as json_file:
             json_file.write(response.text)
 
 def process_csv_files(csv_folder, output_dir):
-    # Ensure the output directory exists, create if not
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Process CSV files concurrently using ThreadPoolExecutor
     with ThreadPoolExecutor() as executor:
         csv_files = [file for file in os.listdir(csv_folder) if file.endswith('.csv')]
         futures = []
@@ -43,16 +31,13 @@ def process_csv_files(csv_folder, output_dir):
             file_path = os.path.join(csv_folder, csv_file)
             futures.append(executor.submit(upload_to_tollguru, file_path, output_dir))
 
-        # Wait for all tasks to complete
         for future in futures:
             future.result()
 
 if __name__ == "__main__":
-    # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Upload GPS tracks to TollGuru API.")
     parser.add_argument("--to_process", required=True, help="Path to the CSV folder.")
     parser.add_argument("--output_dir", required=True, help="The folder where JSON files will be stored.")
     args = parser.parse_args()
 
-    # Process CSV files and upload to TollGuru API
     process_csv_files(args.to_process, args.output_dir)
